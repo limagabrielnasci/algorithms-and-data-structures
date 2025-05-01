f962d868ab7395b9149ae024f27f8e8380004d44
Gabriel Nascimento <limagabrielnasci@gmail.com>
2025-05-01 00:21:02 -0300

    struct exercises

---
+++ programacao_descomplicada/structs/ex003.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    int matricula;
+    char curso[50];
+} Alunos;
+
+Alunos aluno[2];
+
+int main() {
+    /*
+    Construa uma estrutura aluno com nome, numero de matr ´ ´ıcula e curso. Leia do usuario ´
+    a informac¸ao de 5 alunos, armazene em vetor dessa estrutura e imprima os dados na ˜
+    tela
+    */
+    for (int i = 0; i < 2; i++) {
+        printf("dados do aluno %d\n\n", i + 1);
+        printf("matricula: "); scanf("%d", &aluno[i].matricula);
+        getchar();
+        printf("curso: ");
+        fgets(aluno[i].curso, sizeof(aluno->curso), stdin);
+        aluno[i].curso[strcspn(aluno[i].curso, "\n")] = '\0';
+        printf("\n");
+    }
+
+    for (int i = 0; i < 2; i++) {
+        printf("aluno %d\n\n", i + 1);
+        printf("matricula do aluno %d: %d\n", i + 1, aluno[i].matricula);
+        printf("curso: %s\n", aluno[i].curso);
+    }
+}
